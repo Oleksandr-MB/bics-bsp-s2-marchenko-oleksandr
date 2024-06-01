2fffb4d6fd98c041144d6079276e0183389307d4
Oleksandr Marchenko Breneur <146579791+Oleksandr-MB@users.noreply.github.com>
2024-06-01 03:23:58 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,24 @@
 import subprocess
 import os
-
 import tkinter as tk 
 from tkinter import ttk
 from ttkwidgets.autocomplete import AutocompleteCombobox
-
 import tkcalendar as cal
 import datetime
 
+# Define the main class for the application
 class Main:
     def __init__(self, root):
         self.root = root
         self.root.geometry("480x480")  # Set window size
-        self.selected_date = datetime.date.today().strftime("%d/%m/%Y")  # Get today's date
+        self.selected_date = datetime.date.today().strftime("%d/%m/%Y") # Get today's date
         self.initialize_files()  # Initialize file paths
         self.initialize_dropdown_dicts()  # Initialize dropdown dictionaries
         self.create_window_elements()  # Create GUI elements
         self.check_dataset_path()  # Check if dataset path exists
 
     def initialize_files(self):
+        # Initialize the file paths
         self.local_authorities_file = "Dataset/Derived/Text/LocalAuthorities.txt"
         self.counties_file = "Dataset/Derived/Text/Counties.txt"
         self.combined_authorities_file = "Dataset/Derived/Text/CombinedAuthorities.txt"
@@ -26,11 +26,13 @@ class Main:
         self.dataset_path = "Dataset/Derived/"
 
     def initialize_dropdown_dicts(self):
+        # Initialize the dictionaries for the dropdown menus
         self.datasets_d = {"Number of Properties Sold": "ds6", "Median Price": "ds9"}
         self.divisions_d = {"Regions": "1", "Local Authorities": "2", "Counties": "3", "Combined Authorities": "4"}
         self.types_d = {"All": "a", "Detached": "b", "Semi-detached": "c", "Terraced": "d", "Flats and Maisonets": "e"}
 
     def check_dataset_path(self):
+        # Check if the dataset path exists, if not, run the Processor.py script
         ds_path = "Dataset/Derived"
         if not os.path.exists(ds_path):
             script_path = "Scripts/Processor.py"
@@ -38,6 +40,7 @@ class Main:
             process.wait()
         
     def get_locations(self, file):
+        # Get the locations from the file
         with open(file, "r") as f:
             lines = f.readlines()
             if lines:
@@ -49,6 +52,7 @@ class Main:
                 return {} 
             
     def on_division_change(self, event):
+        # Handle the event when the division dropdown changes
         division = self.division_dropdown.get()
         regions = self.get_locations(self.regions_file)
 
@@ -81,6 +85,7 @@ class Main:
         self.on_region_change(None)
 
     def on_region_change(self, event):
+        # Handle the event when the region dropdown changes
         division = self.division_dropdown.get()
         region = self.region_dropdown.get()
         small_divisions = {}
@@ -108,6 +113,7 @@ class Main:
         self.small_divisions = small_divisions
 
     def create_dropdown(self, options, label_text):
+        # Create a dropdown menu
         label = tk.Label(self.root, text=label_text, font=("Georgia", 16))
         label.pack(anchor=tk.W, padx=20)
         
@@ -119,6 +125,7 @@ class Main:
         return dropdown
 
     def create_calendar(self):
+        # Create a calendar widget
         calendar_entry = tk.Entry(self.root)
         today = self.selected_date
         calendar_entry.insert(0, today)
@@ -126,6 +133,7 @@ class Main:
         calendar_entry.pack(anchor=tk.W, padx=20)
 
     def create_window_elements(self):
+        # Create the window elements
         self.dataset_dropdown = self.create_dropdown(self.datasets_d, "Dataset:")
         self.division_dropdown = self.create_dropdown(self.divisions_d, "Division By:")
         self.division_dropdown.bind("<<ComboboxSelected>>", self.on_division_change)
@@ -154,6 +162,7 @@ class Main:
         map_button.pack(side=tk.LEFT, padx=10, pady=50)
 
     def pick_date(self, event, calendar_entry):
+        # Handle the event when a date is picked
         date_window = tk.Toplevel(self.root)
         date_window.title("Select Date")
         
@@ -174,6 +183,7 @@ class Main:
         date_window.bind("<Return>", lambda event: on_date_select())
 
     def open_analyzer(self):      
+        # Open the analyzer script
         dataset = self.datasets_d[self.dataset_dropdown.get()]
         division = self.divisions_d[self.division_dropdown.get()]
 
@@ -184,12 +194,14 @@ class Main:
         subprocess.Popen(["python", script_path, dataset, division, region, location])
 
     def open_map(self):      
+        # Open the map script
         type = self.types_d[self.type_dropdown.get()]
 
         script_path = "Scripts/Map.py"
         subprocess.Popen(["python", script_path, type])
 
     def open_prediction(self):
+        # Open the prediction script
         dataset = self.datasets_d[self.dataset_dropdown.get()]
         division = self.divisions_d[self.division_dropdown.get()]
         type = self.types_d[self.type_dropdown.get()]
